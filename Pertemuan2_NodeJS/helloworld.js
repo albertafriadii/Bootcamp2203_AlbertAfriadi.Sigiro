9f58799e8c1a953d520bc56adf7647f065cdfdc3
afriadialbert <afriadialbert@gmail.com>
2022-03-22 14:17:45 +0700

    update tugas hari kedua

--- Pertemuan2_NodeJS/helloworld.js
+++ Pertemuan2_NodeJS/helloworld.js
@@ -1,3 +1,12 @@
-var namaLengkap = "Albert Afriadi Sigiro";
-var info = "Hai, perkenalkan nama saya " + namaLengkap + ", salam kenal";
-console.log(info);
+var readlineSync = require('readline-sync');
+
+// user memasukkan nama
+var nama = readlineSync.question('Masukkan nama anda: ');
+
+// user memasukkan umur
+var umur = readlineSync.question('Masukkan umur anda: ');
+
+// user memasukkan domisili
+var domisili = readlineSync.question('Masukkan domisili anda: ');
+
+console.log('Perkenalkan nama saya '+ nama + ',saya berusia '+ umur + ' tahun dan domisili saya di '+ domisili + '.');
