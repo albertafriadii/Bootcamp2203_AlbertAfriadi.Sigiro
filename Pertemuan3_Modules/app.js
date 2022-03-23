b92a3cc83c0633dd87c59c0c0d58f8bb7261260c
albertafriadii <albertafriadi01@gmail.com>
2022-03-23 09:45:30 +0700

    Menambahkan file modules

---
+++ Pertemuan3_Modules/app.js
@@ -0,0 +1,27 @@
+// Variable fs
+const fs = require('fs');
+// memasukkan string ke dalam file
+// fs.writeFileSync("test.txt", "Hello world");
+
+// membaca file
+// fs.readFile("test.txt", "utf-8", (err,data) => {
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+// membuat question
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Siapa nama anda? ', (nama) => {
+    rl.question('Berapa nomor handphone anda? ', (nomorHandphone) => {
+        rl.question('Apa alamat email anda? ', (email) => {
+            console.log(`Halo ${nama}, nomor handphone anda ${nomorHandphone} dan alamat email anda adalah ${email}`);
+            rl.close();
+        });
+    });
+});
