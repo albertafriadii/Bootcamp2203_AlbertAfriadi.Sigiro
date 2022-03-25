17550f21ad5fab0fe5f90832cdeec54a2fca06f3
albertafriadii <albertafriadi01@gmail.com>
2022-03-25 09:05:19 +0700

    update comment

--- Pertemuan4_ContactCode/contacts.js
+++ Pertemuan4_ContactCode/contacts.js
@@ -21,19 +21,36 @@ const questions = (ask) => {
 const saveContact = (name, mobile, email) => {
 
     const contact = {name, mobile, email};
+
+    // mengecek folder
     const dirPath = './data';
         if (!fs.existsSync(dirPath)) {
             fs.mkdirSync(dirPath);
+            console.log('Folder tidak ditemukan');
         }
+
+    // membuat folder / file jika belum ada
     const dataPath = './data/contacts.json';
         if (!fs.existsSync(dataPath)) {
             fs.writeFileSync(dataPath, '[]', 'utf-8');
         }
+
+    // membaca file
     const file = fs.readFileSync('data/contacts.json','utf8');
+
+    // membuat file JSON
     const contacts = JSON.parse(file);
+
+    // push data ke contacts melalui variable contact
     contacts.push(contact);
+
+    // menginput / write file data ke JSON melalui variable
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+
+    // menampilkan console log
     console.log('Terima kasih sudah memasukkan data!');
+
+    // menutup readline
     rl.close();
 }
 
