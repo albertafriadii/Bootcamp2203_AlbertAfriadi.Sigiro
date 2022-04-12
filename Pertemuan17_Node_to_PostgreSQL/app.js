f5baf68cc5c3755c7520c22e42cf7bb3e1f2cbaf
albertafriadii <albertafriadi01@gmail.com>
2022-04-12 14:52:29 +0700

    update tugas node to postgres

---
+++ Pertemuan17_Node_to_PostgreSQL/app.js
@@ -0,0 +1,127 @@
+const express = require('express') //module express
+const expressLayouts = require('express-ejs-layouts') //module express layouts
+const path = require('path')    // module path
+
+const app = express() // memanggil fungsi express
+const port = 3000
+const pool = require('./db');
+
+// Setup EJS
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(expressLayouts)
+app.use('/public',express.static(path.join(__dirname, 'public')))
+app.set('layout', './layouts/full-width')
+app.use(express.urlencoded({extended: true}))
+
+// halaman home
+app.get('/', (req, res) => {    // routing page
+
+    const author = "Albert Afriadi Sigiro"
+
+    res.locals.title = "Home"       // menampilkan title
+    res.render('index', {
+        author: author,
+    })      // response untuk dikembalikan ke client
+})
+
+// halaman about
+app.get('/about', (req, res) => {   // routing page
+    res.locals.title = "About"      // menampilkan title
+    res.render('about')   // response untuk dikembalikan ke client
+})
+
+// halaman contact
+app.get('/contact', async(req,res) => {
+    try{
+        const listContact = await pool.query('SELECT * FROM contacts')
+        // res.json(listContact.rows)
+        res.render('contact', {
+            title: "Contact",
+            cont: listContact.rows,
+            listContact,
+        })
+    } catch (err){
+        console.error(err.message)
+    }
+})
+
+// tambah contact
+app.get('/addContact', async(req,res) => {
+    res.render('addContact', {
+        title: "Tambah Contact",
+    })
+})
+
+app.post('/contact', async(req,res) => {
+    try {
+        const {name, mobile, email} = req.body
+        await pool.query(`INSERT INTO contacts VALUES ('${name}','${mobile}','${email}') RETURNING *`)
+        res.redirect('/contact')
+    } catch(err) {
+        console.error(err.message)
+    }
+})
+
+// detail contact
+app.get('/detail/:name', async(req,res) => {
+    try{
+        const name = req.params.name
+        const {rows: detailContact} = await pool.query(`SELECT * FROM contacts WHERE name = '${name}'`)
+        detailContact.map(det => {
+            res.render('detail', {
+                title: "Detail Contact",
+                detailContact,
+                det,
+            })
+        })
+    } catch (err){
+        console.error(err.message)
+    }
+})
+
+// delete contact
+app.get('/contact/delete/:name', async(req,res) => {
+    try {
+        const name = req.params.name
+        const {rows: deleteContact} = await pool.query(`DELETE FROM contacts WHERE name = '${name}'`)
+        res.redirect('/contact')
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// update contact
+app.get('/contact/edit/:name', async(req,res) => {
+    try{
+        const name = req.params.name
+        const {rows: detailContact} = await pool.query(`SELECT * FROM contacts WHERE name = '${name}'`)
+        detailContact.map(det => {
+            res.render('editContact', {
+                title: "Edit Contact",
+                detailContact,
+                det,
+            })
+        })
+    } catch (err){
+        console.error(err.message)
+    }
+})
+
+app.post('/contact/edit', async(req,res) => {
+    try{
+        const {oldData, name, mobile, email} = req.body
+        await pool.query(`UPDATE contacts 
+        SET name = '${name}', mobile = '${mobile}', email = '${email}' 
+        WHERE name = '${oldData}'`)
+        res.redirect('/contact')
+    } catch (err){
+        console.error(err.message)
+    }
+
+})
+
+// listening port / port yang digunakan sebagai server
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ Pertemuan17_Node_to_PostgreSQL/model/contacts.js
@@ -0,0 +1,68 @@
+// const fs = require('fs');
+
+// mengecek folder
+// const dirPath = './data';
+//     if (!fs.existsSync(dirPath)) {
+//         fs.mkdirSync(dirPath);
+//         console.log('Folder tidak ditemukan');
+//     }
+
+// membuat folder / file jika belum ada
+// const dataPath = './data/contacts.json';
+//     if (!fs.existsSync(dataPath)) {
+//         fs.writeFileSync(dataPath, '[]', 'utf-8');
+//     }
+
+// function untuk load
+// const loadContact = () => {
+//     // membaca file
+//     const file = fs.readFileSync('data/contacts.json','utf8');
+
+//     // membaca file JSON
+//     const contacts = JSON.parse(file);
+
+//     return contacts;
+// }
+
+// function untuk tambah data contact
+// const saveContact = (contact) => {
+//     fs.writeFileSync('data/contacts.json', JSON.stringify(contact));
+// }
+
+// const addContact = (contact) => {
+//     const contacts = loadContact()
+//     contacts.push(contact)
+//     saveContact(contacts)
+// }
+
+// function untuk detail data contact
+// const detailContact = (name) => {
+//     const contacts = loadContact();
+//     const detail = contacts.find((contact) => contact.name === name);
+//     return detail
+// }
+
+// const duplicate = (name) => {
+//     const contacts = loadContact();
+//     return contacts.find((contact) => contact.name === name)
+// }
+
+// const deleteContact = (name) => {
+//     const contacts = loadContact()
+//     const deleted = contacts.filter((contact) => contact.name !== name)
+//     saveContact(deleted)
+// }
+
+// const updateContact = (newContact) => {
+//     const contacts = loadContact()
+
+//     const findData = contacts.filter((contact) => contact.name !== newContact.oldData)
+
+//     delete newContact.oldData
+//     findData.push(newContact)
+//     saveContact(findData)
+// }
+
+
+    // membuat export beberapa fungsi ke app.js
+module.exports = {loadContact , detailContact, saveContact, addContact, duplicate, deleteContact, updateContact};
