d7e46265d323d53e1bf59589a8042bcdf9a9a555
albertafriadii <albertafriadi01@gmail.com>
2022-04-13 11:08:21 +0700

    update tugas validasi node to postgres

---
+++ Pertemuan18_Node_to_PostgreSQL_Validation/model/contacts.js
@@ -0,0 +1,77 @@
+// const fs = require('fs');
+
+const pool = require("../db");
+
+// mengecek folder
+// const dirPath = './data';
+//     if (!fs.existsSync(dirPath)) {
+//         fs.mkdirSync(dirPath);
+//         console.log('Folder tidak ditemukan');
+//     }
+
+// membuat folder / file jika belum ada
+// const dataPath = './data/contacts.json';
+//     if (!fs.existsSync(dataPath)) {
+//         fs.writeFileSync(dataPath, '[]', 'utf-8');
+//     }
+
+// function untuk load
+// const loadContact = () => {
+//     // membaca file
+//     const file = fs.readFileSync('data/contacts.json','utf8');
+
+//     // membaca file JSON
+//     const contacts = JSON.parse(file);
+
+//     return contacts;
+// }
+
+// function untuk tambah data contact
+// const saveContact = (contact) => {
+//     fs.writeFileSync('data/contacts.json', JSON.stringify(contact));
+// }
+
+// const addContact = (contact) => {
+//     const contacts = loadContact()
+//     contacts.push(contact)
+//     saveContact(contacts)
+// }
+
+// function untuk detail data contact
+// const detailContact = (name) => {
+//     const contacts = loadContact();
+//     const detail = contacts.find((contact) => contact.name === name);
+//     return detail
+// }
+
+// const duplicate = (name) => {
+//     const contacts = loadContact();
+//     return contacts.find((contact) => contact.name === name)
+// }
+
+// const deleteContact = (name) => {
+//     const contacts = loadContact()
+//     const deleted = contacts.filter((contact) => contact.name !== name)
+//     saveContact(deleted)
+// }
+
+// const updateContact = (newContact) => {
+//     const contacts = loadContact()
+
+//     const findData = contacts.filter((contact) => contact.name !== newContact.oldData)
+
+//     delete newContact.oldData
+//     findData.push(newContact)
+//     saveContact(findData)
+// }
+
+async function checkDelete(value) {
+    try{
+        await pool.query(`DELETE FROM contacts WHERE name = '${value}'`)
+    } catch(err){
+        console.error(err.message)
+    }
+}
+
+    // membuat export beberapa fungsi ke app.js
+module.exports = {checkDelete};
